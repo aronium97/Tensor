70922bbe6ac6201a221c2eb8881b1b2f3102f817
aaron.haag@siemens.com <CSC-cru7F6RPzLtoxd9aPm2N>
2022-01-10 23:02:02 +0100

    - function added

--- myone_missing_data.m
+++ myone_missing_data.m
@@ -31,10 +31,12 @@ R = getR(L,F,nodes);
 
 Y = omega_t*(R*Z + R*A + V);
 
-K = 70; % num iterations
+K = 200; % num iterations
 
 lambda1 = 10%max(max(abs(R'*V)));
 lambdastar = 10%(sqrt(T) + sqrt(F)*sqrt(pi))*sigma%norm(V,1);
+
+mu_soft = 200;
 
 % init P and Q at random
 % X = LxT = PQ'
@@ -43,64 +45,74 @@ P = randn(L,rho);%5*R*A*Q;%randn(L,rho);
 
 
 
-obj_value(1) = getObj(Y,P,Q,R,A, lambdastar, lambda1)%0.5*norm(Y-P*Q'-R*A,'fro').^2 + lambdastar/2*(norm(P,'fro').^2 + norm(Q,'fro').^2) + lambda1*norm(A,1)
 
 
 
-
-for k = 1:K
-    % update the anomaly map
-    Q = Q';
-    % START:
-    %------------------------
-    for t = 1:T
-        P_snake_t = omega_t*P*Q(:,t);
-        D_snake_t = omega_t*R;
-        Y_snake_t = omega_t*Y(:,t);
-    
-        b = -P_snake_t - Y_snake_t;
-        % A = D_snake_t
-        % x = A(:,t)
-    
-        % formula from yang
-        residuum_value = 10^-13;
-        ATA = diag(D_snake_t'*D_snake_t);
-        % entweder diese formel, oder wirkluich die inverse des vektors!
-        
-        r = ATA.*A(:,t) - D_snake_t'*(D_snake_t*A(:,t) - b);
-
-        mu_soft = 1000;
-        Bx = (ATA.^-1).*soft_operator(r,mu_soft);
-
-        gamma = min(1,max(0,-(D_snake_t*A(:,t) - b)'*D_snake_t*(Bx - A(:,t)) + mu_soft*(norm(Bx, 1) - norm(A(:,t),1)) / ( (D_snake_t*(Bx - A(:,t)))' * (D_snake_t*(Bx - A(:,t))) )));
-
-        A_new(:,t) = A(:,t) + gamma*(Bx -A(:,t));
-    end
-
-    A = A_new;
-
-
-    
-    % END
-    Q = Q';
-    %------------------------
-
-
-    % update the nominal traffic subspace:
-    for l = 1:L
-        P(l,:) = inv(lambdastar*eye(rho) + Q'*omega_l*Q) * Q'*omega_l*(Y(l,:)' - A'*R(l,:)');
-    end
-
-    % update the projection coefficients
-    for t = 1:T
-        Q(t,:) = inv(lambdastar*eye(rho) + P'*omega_t*P)*P'*omega_t*(Y(:,t) - R*A(:,t));
-    end
-
-    obj_value(k+1) = getObj(Y,P,Q,R,A, lambdastar, lambda1)%0.5*norm(Y-P*Q'-R*A,'fro').^2 + lambdastar/2*(norm(P,'fro').^2 + norm(Q,'fro').^2) + lambda1*norm(A,1)
-end
+obj_value = bsca_missing_data(P, Q, A, K, R, Y, omega_t, omega_l, lambdastar, lambda1, mu_soft);
 
 plot(obj_value, "--")
 set(gca, 'YScale', 'log')
+
+
+function obj_value = bsca_missing_data(P, Q, A, K, R, Y, omega_t, omega_l, lambdastar, lambda1, mu_soft)
+
+    T = size(Y,2);
+    L = size(P,1);
+    rho = size(Q,2);
+
+
+    obj_value(1) = getObj(Y,P,Q,R,A, lambdastar, lambda1)
+
+    for k = 1:K
+        % update the anomaly map
+        Q = Q';
+        % START:
+        %------------------------
+        for t = 1:T
+            P_snake_t = omega_t*P*Q(:,t);
+            D_snake_t = omega_t*R;
+            Y_snake_t = omega_t*Y(:,t);
+        
+            b = -P_snake_t - Y_snake_t;
+            % A = D_snake_t
+            % x = A(:,t)
+        
+            % formula from yang
+            ATA = diag(D_snake_t'*D_snake_t);
+            
+            r = ATA.*A(:,t) - D_snake_t'*(D_snake_t*A(:,t) - b);
+    
+            mu_soft = 100;
+
+            Bx = (ATA.^-1).*soft_operator(r,mu_soft);
+    
+            gamma = min(1,max(0,-(D_snake_t*A(:,t) - b)'*D_snake_t*(Bx - A(:,t)) + mu_soft*(norm(Bx, 1) - norm(A(:,t),1)) / ( (D_snake_t*(Bx - A(:,t)))' * (D_snake_t*(Bx - A(:,t))) )));
+    
+            A_new(:,t) = A(:,t) + gamma*(Bx -A(:,t));
+        end
+    
+        A = A_new;
+        
+        % END
+        Q = Q';
+        %------------------------
+    
+    
+        % update the nominal traffic subspace:
+        for l = 1:L
+            P(l,:) = inv(lambdastar*eye(rho) + Q'*omega_l*Q) * Q'*omega_l*(Y(l,:)' - A'*R(l,:)');
+        end
+    
+        % update the projection coefficients
+        for t = 1:T
+            Q(t,:) = inv(lambdastar*eye(rho) + P'*omega_t*P)*P'*omega_t*(Y(:,t) - R*A(:,t));
+        end
+    
+        obj_value(k+1) = getObj(Y,P,Q,R,A, lambdastar, lambda1)
+    end
+end
+
+
 
 
 
